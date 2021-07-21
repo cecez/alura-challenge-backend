e64631c5ff24fb30c3f519907cfd4cca67038220
Cezar Rosa <cezar@nitrodev.com>
2021-07-20 23:37:50 -0300

    - endpoints para listar todos vídeos, um vídeo, criar um vídeo, atualizar um vídeo e excluir um vídeo - validando entradas de dados - formatando respostas da API com Resources

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\VideoCollection;
+use App\Http\Resources\VideoResource;
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return VideoCollection
+     */
+    public function index()
+    {
+        return new VideoCollection(Video::all());
+//        return new VideoCollection(Video::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return VideoResource
+     */
+    public function store(Request $request)
+    {
+        // valida
+        $dadosValidados = $request->validate([
+            'titulo' => ['required', 'max:255'],
+            'descricao' => ['required', 'max:255'],
+            'url' => ['required', 'max:255', 'url'],
+        ]);
+
+        // salva
+        $video = Video::create($dadosValidados);
+
+        // retorna
+        return new VideoResource($video);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Video  $video
+     * @return VideoResource
+     */
+    public function show(Video $video)
+    {
+        return new VideoResource($video);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Video  $video
+     * @return VideoResource
+     */
+    public function update(Request $request, Video $video)
+    {
+        // valida
+        $dadosValidados = $request->validate([
+             'titulo' => ['sometimes', 'required', 'max:255'],
+             'descricao' => ['sometimes', 'required', 'max:255'],
+             'url' => ['sometimes', 'required', 'max:255', 'url'],
+         ]);
+
+        // atualiza
+        if (!empty($dadosValidados)) {
+            $video->update($dadosValidados);
+        }
+
+        // retorna
+        return new VideoResource($video);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Video  $video
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Video $video)
+    {
+        if ($video->delete()) {
+            return response('Video excluído com sucesso.');
+        } else {
+            return response('Falha ao excluir vídeo.');
+        }
+    }
+}
