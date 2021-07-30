066203cbb8aff336990e1448d88c9b6d4e88ce49
Cezar Rosa <cezar@nitrodev.com>
2021-07-29 22:49:45 -0300

    ajustando para que feature tests sejam executados com banco de dados sqlite em memória, sem seeds por padrão

--- tests/Feature/Api/VideoTest.php
+++ tests/Feature/Api/VideoTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature\Api;
 
+use Database\Seeders\VideoSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class VideoTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected bool $seed = false;
 
     /**
      * Teste para GET /videos
@@ -22,8 +25,10 @@ class VideoTest extends TestCase
     /**
      * Teste para GET /videos/{id}, caso de vídeo que existe
      */
-    public function test_get_videos_1_existente(): void
+    public function test_get_video_existente(): void
     {
+        $this->seed(VideoSeeder::class);
+
         $response = $this->getJson('/api/videos/1');
 
         $response->assertStatus(200);
@@ -32,7 +37,7 @@ class VideoTest extends TestCase
     /**
      * Teste para GET /videos/{x}, caso de vídeo que não existe
      */
-    public function test_get_videos_1_nao_existente(): void
+    public function test_get_video_nao_existente(): void
     {
         $response = $this->getJson('/api/videos/123123123');
 
@@ -44,7 +49,7 @@ class VideoTest extends TestCase
     /**
      * Teste para POST /videos, inserindo vídeo com sucesso
      */
-    public function test_post_videos_com_sucesso()
+    public function test_post_video_com_sucesso()
     {
         $response = $this->postJson('/api/videos', [
             'titulo' => 'um título',
@@ -60,7 +65,7 @@ class VideoTest extends TestCase
     /**
      * Teste para POST /videos, com falha ao inserir vídeo, barrado na validação
      */
-    public function test_post_videos_sem_sucesso()
+    public function test_post_video_sem_sucesso()
     {
         $response = $this->postJson('/api/videos', [
             'descricao' => 'uma descrição',
@@ -75,8 +80,10 @@ class VideoTest extends TestCase
     /**
      * Teste para PUT /videos/1, atualizando vídeo com sucesso
      */
-    public function test_put_videos_1_com_sucesso()
+    public function test_put_video_com_sucesso()
     {
+        $this->seed(VideoSeeder::class);
+
         $response = $this->putJson('/api/videos/1', [
             'titulo' => 'um título atualizado',
             'descricao' => 'uma descrição atualizada',
@@ -91,8 +98,10 @@ class VideoTest extends TestCase
     /**
      * Teste para PUT /videos/1, com falha ao atualizar vídeo, barrado na validação
      */
-    public function test_put_videos_1_sem_sucesso()
+    public function test_put_video_sem_sucesso()
     {
+        $this->seed(VideoSeeder::class);
+
         $response = $this->putJson('/api/videos/1', [
             'descricao' => '',
             'url' => 'urlinexistente'
@@ -106,8 +115,10 @@ class VideoTest extends TestCase
     /**
      * Teste para DELETE /videos/1, caso de vídeo que existe
      */
-    public function test_delete_videos_1_existente(): void
+    public function test_delete_video_existente(): void
     {
+        $this->seed(VideoSeeder::class);
+
         $response = $this->deleteJson('/api/videos/1');
 
         $response->assertStatus(200);
@@ -116,7 +127,7 @@ class VideoTest extends TestCase
     /**
      * Teste para DELETE /videos/{x}, caso de vídeo que não existe
      */
-    public function test_delete_videos_1_nao_existente(): void
+    public function test_delete_video_nao_existente(): void
     {
         $response = $this->deleteJson('/api/videos/123123123');
 
